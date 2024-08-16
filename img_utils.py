4a2f24418f78ffe96fb96d2a25d7ad19acf86da4
dSamp41 <d.sampietro@studenti.unipi.it>
2024-08-16 19:46:37 +0200

    Update img_utils.py

--- img_utils.py
+++ img_utils.py
@@ -1,17 +1,28 @@
+from dataclasses import dataclass
 from ascii_magic import AsciiArt #type: ignore
 from PIL import Image, ImageFont, ImageDraw #type: ignore
+
+@dataclass
+class Info:
+    textCols: int
+    textRows: int
+    
 
 def img_to_text(inputPath: str, outputPath: str, cols=120) -> None:
     img = AsciiArt.from_image(inputPath)
     img.to_file(path=outputPath, columns=cols, monochrome=True) #TODO: remove monochrome
 
 
-def save_img_to_file(inputPath: str, outputPath: str) -> None:
+def save_img_to_file(inputPath: str, outputPath: str, fontsize: int, info: Info) -> None:
     with open(inputPath, "r") as f:
         ascii_text: str = f.read()
 
-    img = Image.new('RGB', (1200, 600), color = (255,255,255))
-    fnt = ImageFont.truetype("fonts/Consolas.ttf", 15)
-    ImageDraw.Draw(img).text((0,0), ascii_text, font=fnt, fill=(0,0,0))
+    img_width = 1920
+    img_heigth = fontsize * info.textRows
+    img = Image.new('RGB', (img_width, img_heigth), color = (255,255,255))
+    fnt = ImageFont.truetype("fonts/Consolas.ttf", fontsize)
+    draw = ImageDraw.Draw(img)
+    
+    draw.text((0,0), ascii_text, font=fnt, fill=(0,0,0))
 
     img.save(outputPath)
