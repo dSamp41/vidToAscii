db8dac25524ba957347bcb69c3a6836166742f41
dSamp41 <d.sampietro@studenti.unipi.it>
2024-08-18 18:54:52 +0200

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -1,10 +1,16 @@
 import os
-from img_utils import Info, img_to_text, save_img_to_file #type: ignore
+from coloredTextToImg import assemble_text_colormap
+from img_utils import img_to_text #type: ignore
+from parsing import text_to_colormap
+from utils import ImageInfo, TextInfo
 from video_utils import collect_imgs_to_video, extract_frames
 
 INPUT_PATH = "images/input"
 OUTPUT_PATH = "images/output"
+MAPPINGS_PATH = "images/mappings"
+
 FRAMES_FOLDER = "frames"
+FRAMES_ASCII_FOLDER = "frames_ascii"
 
 
 def clear_folder(folder: str) -> None:
@@ -12,19 +18,32 @@ def clear_folder(folder: str) -> None:
         os.remove(f"{folder}/{file}")
 
 
-COLUMNS = 200
-INFO = Info(200, 51)
 if __name__ == "__main__":
+    IMAGE_INFO = ImageInfo(1920, 1080)
+
+    COLS = 700
+    ROWS = int((IMAGE_INFO.height * COLS) / (IMAGE_INFO.width * IMAGE_INFO.width_ratio))
+    TEXT_INFO = TextInfo(COLS, ROWS)
+
     # video -> frames (pngs)
     extract_frames("images/input/akira_slide.mp4")
 
     # frame -> ascii img
     for img in os.listdir(FRAMES_FOLDER):
         filename = img.split(".")[0]
-        img_to_text(f"{FRAMES_FOLDER}/{img}", f"{OUTPUT_PATH}/{filename}.txt", COLUMNS)
-        save_img_to_file(f"{OUTPUT_PATH}/{filename}.txt", f"{OUTPUT_PATH}/{filename}_ascii.png", 15, INFO)
 
-        os.remove(f"{OUTPUT_PATH}/{filename}.txt")
+        #frame png -> text
+        img_to_text(f"{FRAMES_FOLDER}/{img}", f"{MAPPINGS_PATH}/{filename}.txt", IMAGE_INFO, TEXT_INFO)
+
+        #processing
+        text_to_colormap(f"{MAPPINGS_PATH}/{filename}.txt", f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt")
+        
+        #overlap text and color        
+        assemble_text_colormap(f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt", f"{FRAMES_ASCII_FOLDER}/{filename}.png", 15, TEXT_INFO)
 
     # asciis -> video
-    collect_imgs_to_video("akira_slide_ascii.mp4", f"{OUTPUT_PATH}")
+    collect_imgs_to_video("akira_slide_ascii.mp4", FRAMES_ASCII_FOLDER, 24, IMAGE_INFO)
+
+#clean-up
+for folder in [FRAMES_FOLDER, FRAMES_ASCII_FOLDER, MAPPINGS_PATH]:
+    clear_folder(folder)
