7e65b00f5b813b54c0537fe251d4e0b2106cb194
dSamp41 <d.sampietro@studenti.unipi.it>
2024-08-18 18:54:36 +0200

    Update coloredTextToImg.py

--- coloredTextToImg.py
+++ coloredTextToImg.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
-from img_utils import Info #type: ignore
+from utils import ImagePath, TextInfo, TextPath
 
 COLORS_CODE = {
     'B': (0, 0, 0), #black
@@ -10,49 +10,49 @@ COLORS_CODE = {
     'm': (255, 0, 255), #magenta
     'c': (0, 255, 255), #cyan
     'W': (255, 255, 255), #white
-    'T': "white",  #transparent
+    'T': (102, 102, 102),  #transparent
     '\n': 'white'
 }
 
-#TODO: \n is problematic
-def save_img_to_file(inputPath: str, colormapPath: str, outputPath: str, fontsize: int, info: Info) -> None:
-    with open(inputPath, "r") as f:
+def assemble_text_colormap(charsPath: TextPath, colormapPath: TextPath, outputPath: ImagePath, fontsize: int, info: TextInfo) -> None:
+    with open(charsPath, "r") as f:
         ascii_text: str = f.read()
-
-    print(ascii_text[200] == '\n')
 
     with open(colormapPath, 'r') as f:
         colormap: str = f.read()
     
-    #colormap: list[str] = colormap.split('\n')
+    FONT_WIDTH = 8.25 #@15 px fontsize
     
     colormap: list[tuple] = list(map(lambda x: COLORS_CODE[x], "".join(colormap)))
 
-    img_width = 1920
+    img_width = int(FONT_WIDTH * info.textCols)
     img_heigth = fontsize * info.textRows
     img = Image.new('RGB', (img_width, img_heigth), color = "black")
+
     fnt = ImageFont.truetype("fonts/Consolas.ttf", fontsize)
     draw = ImageDraw.Draw(img)
     
     (x_pos, y_pos) = (0, 0)
 
     for n in range(info.textRows):
-        for i in range(info.textCols + 1):
-            index = n*info.textCols + i
-            toWrite = ascii_text[index]
+        for i in range(info.textCols):
+            try:
+                index = n*(info.textCols) + i   # +1 because of \n column
+                toWrite = ascii_text[index]
 
-            x_pos = i * 8.25
-            draw.text((x_pos, y_pos), toWrite, font=fnt, fill=colormap[index])
+                x_pos = i * FONT_WIDTH
+                draw.text((x_pos, y_pos), toWrite, font=fnt, fill=colormap[index])
 
-            #print(f"{n} row {i} col -> {toWrite}")
+                #print(f"{n} row {i} col -> {toWrite}")
+            except IndexError:
+                break
 
         x_pos = 0
         y_pos += fontsize
-
 
     img.save(outputPath)
     
 
 if __name__ == "__main__":
-    INFO = Info(200, 51)
-    save_img_to_file("monochrome-frame-0000.txt", "colors-frame-0000.txt", "multicolor-test.png", 15, INFO)
+    INFO = TextInfo(200, 63)
+    assemble_text_colormap("test/chars-frame-0000.txt", "test/colors-frame-0000.txt", "test/multicolor-frame-0000.png", 15, INFO)
