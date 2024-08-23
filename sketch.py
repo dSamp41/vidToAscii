929a4e3ad8196666a4f41bd5360f43526158a093
dSamp41 <d.sampietro@studenti.unipi.it>
2024-08-23 18:44:35 +0200

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -2,8 +2,8 @@ import os
 from coloredTextToImg import assemble_text_colormap
 from img_utils import img_to_text #type: ignore
 from parsing import text_to_colormap
-from utils import ImageInfo, TextInfo
-from video_utils import collect_imgs_to_video, extract_frames
+from stucts import ImageInfo, Info, TextInfo
+from video_utils import collect_imgs_to_video, extract_frames, get_size
 
 INPUT_PATH = "images/input"
 OUTPUT_PATH = "images/output"
@@ -19,31 +19,39 @@ def clear_folder(folder: str) -> None:
 
 
 if __name__ == "__main__":
-    IMAGE_INFO = ImageInfo(1920, 1080)
+    filename = "akira_slide"
+    VIDEO = f"{INPUT_PATH}/{filename}.mp4"
+    COLS = 500
+    
+    INFO = Info(TextInfo(COLS), get_size(VIDEO))
 
-    COLS = 700
-    ROWS = int((IMAGE_INFO.height * COLS) / (IMAGE_INFO.width * IMAGE_INFO.width_ratio))
-    TEXT_INFO = TextInfo(COLS, ROWS)
+    IMAGE_INFO: ImageInfo = INFO.imgInfo
+    TEXT_INFO: TextInfo = INFO.txtInfo
 
-    # video -> frames (pngs)
-    extract_frames("images/input/akira_slide.mp4")
 
-    # frame -> ascii img
-    for img in os.listdir(FRAMES_FOLDER):
-        filename = img.split(".")[0]
+    try:
+        # video -> frames (pngs)
+        extract_frames(VIDEO, FRAMES_FOLDER)
 
-        #frame png -> text
-        img_to_text(f"{FRAMES_FOLDER}/{img}", f"{MAPPINGS_PATH}/{filename}.txt", IMAGE_INFO, TEXT_INFO)
+        # forall frame -> ascii img
+        for img in os.listdir(FRAMES_FOLDER):
+            filename = img.split(".")[0]
 
-        #processing
-        text_to_colormap(f"{MAPPINGS_PATH}/{filename}.txt", f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt")
-        
-        #overlap text and color        
-        assemble_text_colormap(f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt", f"{FRAMES_ASCII_FOLDER}/{filename}.png", 15, TEXT_INFO)
+            #frame png -> text
+            img_to_text(f"{FRAMES_FOLDER}/{img}", f"{MAPPINGS_PATH}/{filename}.txt", IMAGE_INFO, TEXT_INFO)
 
-    # asciis -> video
-    collect_imgs_to_video("akira_slide_ascii.mp4", FRAMES_ASCII_FOLDER, 24, IMAGE_INFO)
-
-#clean-up
-for folder in [FRAMES_FOLDER, FRAMES_ASCII_FOLDER, MAPPINGS_PATH]:
-    clear_folder(folder)
+            #processing
+            text_to_colormap(f"{MAPPINGS_PATH}/{filename}.txt", f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt")
+            
+            #overlap text and color        
+            assemble_text_colormap(f"{MAPPINGS_PATH}/chars-{filename}.txt", f"{MAPPINGS_PATH}/colors-{filename}.txt", f"{FRAMES_ASCII_FOLDER}/{filename}.png", 15, TEXT_INFO)
+            
+        # asciis -> video
+        collect_imgs_to_video(f"{OUTPUT_PATH}/{filename}_ascii.mp4", FRAMES_ASCII_FOLDER, 24, IMAGE_INFO)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("cleaning...")
+        #clean-up
+        for folder in [FRAMES_FOLDER, FRAMES_ASCII_FOLDER, MAPPINGS_PATH]:
+            clear_folder(folder)
